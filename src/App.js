6cd474158386b9a5ca0e0c75c8fece3984665493
aschmidt20 <schmi651@msu.edu>
2019-08-13 17:33:16 -0400

    Testing netlify continuous deployment

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Continuous Deployment Test
         </a>
       </header>
     </div>
